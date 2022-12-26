f72bec9a0fb2813806cced4cdd49707387c4b4ee
david-ricardo <contact@davidrica.com>
2022-12-26 15:14:10 +0700

    Add GoogleMap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
-import Button from "@mui/material/Button";
+import { GoogleMap } from "./components";
+import { Container, Grid } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="contained">Hello World</Button>
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <GoogleMap />
+            </Grid>
+          </Grid>
+        </Container>
       </header>
     </div>
   );
